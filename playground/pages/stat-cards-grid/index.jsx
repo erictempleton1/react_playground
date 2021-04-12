65540856890c2b95e7ff8ccc4eb423c21039101b
Eric Templeton <eric.templeton@parsons.com>
2021-04-12 09:28:55 -0400

    finishing up basic cards

--- playground/pages/stat-cards-grid/index.jsx
+++ playground/pages/stat-cards-grid/index.jsx
@@ -3,13 +3,20 @@ import styles from './stat-cards-grid.module.css';
 const Index = () => (
   <div className={styles.Container}>
     <div className={styles.Grid__Container}>
-      <Card />
+      <Card title="Clicks" content="2,398" />
+      <Card title="Impressions" content="62,156" />
+      <Card title="CTR" content="3.9%" />
     </div>
   </div>
 );
 
-const Card = () => (
-  <div className={styles.Card}></div>
-)
+const Card = ({ title, content }) => (
+  <div className={styles.Stat__Card}>
+    <div className={styles.Stat__Container}>
+      <div className={styles.Stat__Title}>{title}</div>
+      <div className={styles.Stat__Content}>{content}</div>
+    </div>
+  </div>
+);
 
 export default Index;
--- playground/pages/stat-cards-grid/notes.md
+++ playground/pages/stat-cards-grid/notes.md
@@ -5,6 +5,13 @@
 * might not need `grid-template-rows`
     * just use spacing from the card itself
 * svg icons would be nice to use, but add some complexity
+* cards look really nice on a light grey background
+* start with a basic version and build from there
+* this example uses three cards, but if you had four you could move to two columns at a smaller width than 768px. 
+  * not sure how common devices sized 425px to 768px are, so might not be needed
+* plain and simple. conveys the info clearly without a lot of ceremony.l
+  * some sites need plain and simple.
+  * getting the summary without fuss can be a feature
 
 ##### Example stats
 * clicks
